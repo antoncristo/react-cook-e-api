6cf1896486d9aea6fa3cee0c5f0c73fcc8556f38
Anton Cristo <antoncristo92@gmail.com>
2023-10-06 11:10:41 +0300

    global error respondes with BAD_REQUEST

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use('/api', appRouter);
 
 // not found/bad request/error handling section
 app.use('*', (req, res) => {
-	res.send('Not Found').status(404);
+	res.status(500).send();
 });
 
 app.listen(SERVER_PORT, '0.0.0.0', () =>

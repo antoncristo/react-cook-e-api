5299b08c84fc92909fb95037f5a0d76b7d9cde62
Anton Cristo <antoncristo92@gmail.com>
2023-10-12 12:56:22 +0300

    firebase setup - initial

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,5 @@ app.use('*', (req, res) => {
 app.listen(SERVER_PORT, '0.0.0.0', () =>
 	console.log('[Server][Connection]: listening on port', SERVER_PORT)
 );
+
+console.log('[test render env]:', process.env.FIREBASE_PROJECT_ID);

6a72dee9172b84d9f91437d21fc78362ad56f09a
Anton Cristo <antoncristo92@gmail.com>
2023-10-18 16:52:37 +0300

    abstract auth provider away from auth service

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { randomUUID } from 'crypto';
-import { FirebaseAuthProvider, SignInParams, SignInResponse } from 'firebase/auth';
+import { FirebaseAuthProvider, SignInParams } from 'firebase/auth';
 import { AuthServiceApi } from './auth.contract';
-import { parseFirebaseUserToCookeUser } from 'firebase/utils';
 import { verifyToken } from 'utils/jwt';
 
 class AuthService implements AuthServiceApi {
@@ -11,31 +10,10 @@ class AuthService implements AuthServiceApi {
 		email: Email;
 		password: Password;
 		name: string;
-	}): Promise<CookEUser> =>
-		this.authProvider.admin
-			.createUser({
-				uid: randomUUID(),
-				email: credentials.email,
-				password: credentials.password,
-				displayName: credentials.name
-			})
-			.then(fbUser => parseFirebaseUserToCookeUser(fbUser));
+	}): Promise<CookEUser> => this.authProvider.createUser(credentials);
 
 	signInWithEmailAndPassword = async (credentials: SignInParams): Promise<CookEUser> =>
-		this.authProvider.client
-			.signInWithEmailAndPAssword({
-				email: credentials.email,
-				password: credentials.password
-			})
-			.then(response => {
-				const user: CookEUser = {
-					email: response.data.email,
-					name: response.data.email,
-					uuid: response.data.localId
-				};
-
-				return user;
-			});
+		this.authProvider.signIn(credentials);
 
 	getUserFromBearer = async (bearerToken: string): Promise<CookEUser> => {
 		try {
--- src/firebase/auth/authProvider.ts
+++ src/firebase/auth/authProvider.ts
@@ -1,7 +1,37 @@
-import { FirebaseRestClient } from './REST';
+import { randomUUID } from 'crypto';
+import { FirebaseRestClient, SignInParams } from './REST';
 import { auth } from './admin';
+import { parseFirebaseUserToCookeUser } from 'firebase/utils';
 
 export class FirebaseAuthProvider {
-	admin = auth;
-	client = new FirebaseRestClient();
+	private admin = auth;
+	private client = new FirebaseRestClient();
+
+	async createUser(credentials: { email: Email; password: Password; name: string }) {
+		return this.admin
+			.createUser({
+				uid: randomUUID(),
+				email: credentials.email,
+				password: credentials.password,
+				displayName: credentials.name
+			})
+			.then(fbUser => parseFirebaseUserToCookeUser(fbUser));
+	}
+
+	async signIn(credentials: SignInParams) {
+		return this.client
+			.signInWithEmailAndPAssword({
+				email: credentials.email,
+				password: credentials.password
+			})
+			.then(response => {
+				const user: CookEUser = {
+					email: response.data.email,
+					name: response.data.email,
+					uuid: response.data.localId
+				};
+
+				return user;
+			});
+	}
 }

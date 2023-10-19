c476b4430e323dc9f2f0084c6af5d79a4e6525da
Anton Cristo <antoncristo92@gmail.com>
2023-10-19 08:23:34 +0300

    recipes service using firebase db provider abstraction

--- src/api/recipes/recipes.controller.ts
+++ src/api/recipes/recipes.controller.ts
@@ -3,11 +3,15 @@ import { QueryBasicParams } from 'api/types';
 import { CookError } from 'errors';
 import { recipesService } from './recipes.service';
 import { Recipe } from './recipes.type';
+import { extractTokenFromAuthHeader, getUserFromBearer } from 'utils/jwt';
 
 export const getRecipes: RequestHandler = async (req, res, next) => {
 	try {
 		const queryParams: QueryBasicParams = req.query;
-		const recipes = await recipesService.getRecipes(queryParams);
+		const user = await getUserFromBearer(
+			extractTokenFromAuthHeader(req.headers.authorization)!
+		);
+		const recipes = await recipesService.getRecipes(queryParams, user.uuid);
 
 		res.send(recipes);
 	} catch (err) {
@@ -18,7 +22,10 @@ export const getRecipes: RequestHandler = async (req, res, next) => {
 export const postRecipe: RequestHandler = async (req, res, next) => {
 	try {
 		const recipePayload = req.body.recipe as Recipe;
-		const success = await recipesService.postRecipe(recipePayload);
+		const user = await getUserFromBearer(
+			extractTokenFromAuthHeader(req.headers.authorization)!
+		);
+		const success = await recipesService.postRecipe(recipePayload, user.uuid);
 
 		res.send(success);
 	} catch (err) {
@@ -29,7 +36,10 @@ export const postRecipe: RequestHandler = async (req, res, next) => {
 export const deleteRecipe: RequestHandler = async (req, res, next) => {
 	try {
 		const recipeIDToDelete = req.params.recipeid as UUID;
-		const success = await recipesService.deleteRecipe(recipeIDToDelete);
+		const user = await getUserFromBearer(
+			extractTokenFromAuthHeader(req.headers.authorization)!
+		);
+		const success = await recipesService.deleteRecipe(recipeIDToDelete, user.uuid);
 
 		res.send({ deleted: success });
 	} catch (err) {
@@ -40,8 +50,11 @@ export const deleteRecipe: RequestHandler = async (req, res, next) => {
 export const putRecipe: RequestHandler = async (req, res, next) => {
 	try {
 		const updatedRecipe = req.body.recipe as Recipe;
+		const user = await getUserFromBearer(
+			extractTokenFromAuthHeader(req.headers.authorization)!
+		);
 
-		const success = await recipesService.putRecipe(updatedRecipe);
+		const success = await recipesService.putRecipe(updatedRecipe, user.uuid);
 
 		res.send(success);
 	} catch (err) {

da3f996f0fe996ef453a92914c0f45a351b8ae56
Anton Cristo <antoncristo92@gmail.com>
2023-10-20 15:10:12 +0300

    dbprovider class encapsulates document dals

---
+++ src/firebase/db/dal/preferences-dal.ts
@@ -0,0 +1,26 @@
+import { Preferences } from 'api/preferences/preferences.type';
+import { db } from '../admin';
+
+const ROOT_COLLECTION = 'user-preferences';
+
+export class PreferencesDal {
+	private admin = db;
+
+	async setPreferences(userID: UUID, preferences: Preferences) {
+		return this.admin
+			.collection(ROOT_COLLECTION)
+			.doc(userID)
+			.set(preferences)
+			.then(() => preferences);
+	}
+
+	async getPreferences(userID: UUID): Promise<Preferences> {
+		return this.admin
+			.collection(ROOT_COLLECTION)
+			.doc(userID)
+			.get()
+			.then(snapshot => {
+				return snapshot.data() as Preferences;
+			});
+	}
+}
--- src/firebase/db/db-provider.ts
+++ src/firebase/db/db-provider.ts
@@ -1,59 +1,6 @@
-import { Recipe } from 'api/recipes/recipes.type';
-import { db } from './admin';
+import { PreferencesDal, RecipeDal } from './dal';
 
 export class FirebaseDatabaseProvider {
-	private admin = db;
-
-	async addRecipe(recipe: Recipe, userID: string) {
-		return this.admin
-			.collection('users')
-			.doc(userID)
-			.collection('recipes')
-			.doc(recipe.id)
-			.set(recipe);
-	}
-
-	async getRecipes(userID: string) {
-		return this.admin
-			.collection('users')
-			.doc(userID)
-			.collection('recipes')
-			.get()
-			.then(snapshot => {
-				const recipes: Recipe[] = [];
-				snapshot.forEach(rec => recipes.push(rec.data() as Recipe));
-				return recipes;
-			})
-			.then(arr => arr);
-	}
-
-	async getRecipe(recipeID: UUID, userID: UUID): Promise<Recipe | undefined> {
-		return this.admin
-			.collection('users')
-			.doc(userID)
-			.collection('recipes')
-			.doc(recipeID)
-			.get()
-			.then(snapshot => {
-				return snapshot.data() as Recipe;
-			});
-	}
-
-	async deleteRecipe(recipeID: string, userID: string) {
-		return this.admin
-			.collection('users')
-			.doc(userID)
-			.collection('recipes')
-			.doc(recipeID)
-			.delete();
-	}
-
-	async updateRecipe(recipe: Recipe, userID: string) {
-		return this.admin
-			.collection('users')
-			.doc(userID)
-			.collection('recipes')
-			.doc(recipe.id)
-			.update({ ...recipe });
-	}
+	recipes = new RecipeDal();
+	preferences = new PreferencesDal();
 }
